b90722079d66627c557f4014341103746bd250e9
ShikiBo(房士琦) <shiki.bo.f704@dirbato.co.jp>
2024-04-25 12:37:53 +0900

    user情報の変更機能を作成

---
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.techacademy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.service.UserService;
+import com.techacademy.entity.User;
+
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService service;
+    
+    @GetMapping("/list")
+    public String getList(Model model) {
+        // 全件検索結果をModelに登録
+        model.addAttribute("userlist", service.getUserList());
+        // user/list.htmlに画面遷移
+        return "user/list";
+    }
+    
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+    
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+}
