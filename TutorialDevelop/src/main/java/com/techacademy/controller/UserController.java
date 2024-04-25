856799d4c778675d1a11e8037b613a1f892b83d5
ShikiBo(房士琦) <shiki.bo.f704@dirbato.co.jp>
2024-04-25 14:05:14 +0900

    user削除機能を作成

--- TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.techacademy.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; 
 
 import com.techacademy.service.UserService;
 import com.techacademy.entity.User;
@@ -37,6 +40,14 @@ public class UserController {
     public String postRegister(User user) {
         // User登録
         service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
